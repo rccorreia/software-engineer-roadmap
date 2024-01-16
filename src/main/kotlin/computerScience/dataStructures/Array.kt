f4792bf44b68ee911add5d7d5b921290ce8a1c6a
Ricardo Correia <rdcc89@gmail.com>
2024-01-16 02:09:51 -0300

    fix fun firstNotRepeatingCharacter to use array method

--- src/main/kotlin/computerScience/dataStructures/Array.kt
+++ src/main/kotlin/computerScience/dataStructures/Array.kt
@@ -90,8 +90,8 @@ class ArrayInterviewQuestions{
 // }
 
     fun firstNotRepeatingCharacter(s: String) =
-        s.groupingBy { it }.eachCount()
-            .filter { it.value == 1 }.toList().firstOrNull()?.first ?: '_'
+        arrayOf(s.groupingBy { it }.eachCount()
+            .filter { it.value == 1 }).firstOrNull()?.keys?.firstOrNull() ?: '_'
 
 
 //    You are given an n x n 2D matrix that represents an image. Rotate the image by 90 degrees (clockwise).
